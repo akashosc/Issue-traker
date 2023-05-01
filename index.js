74a9e45d821ec1b372288561cf36a48b5b7fcdb6
akashosc <84351117+akashosc@users.noreply.github.com>
2023-05-02 00:44:35 +0530

    adding mongodb in

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const path=require('path');
 const ejs =require('ejs');
 const expressLayouts=require('express-ejs-layouts');
 const app=express();
-const PORT=process.env.PORT||3000;
-
+const PORT=3000;
+const mongoUrl='mongodb+srv://akash7067tiwari:cduFDdZS9juSDHC7@cluster0.vqewvkk.mongodb.net/?retryWrites=true&w=majority'
 
 // mongoose connect object
-mongoose.connect(process.env.mongoUrl,{useNewUrlParser:true}).then(()=>console.log('MongoDb connected'))
+mongoose.connect(mongoUrl,{useNewUrlParser:true}).then(()=>console.log('MongoDb connected'))
 .catch(err=>console.log(err));
 
 app.use(express.urlencoded({extended:true}));
